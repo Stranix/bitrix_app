44ceba410b6c8ae654a7c9e5703061b4df625d5d
Константин Смуров <Phantom2525@gmail.com>
2023-04-27 14:12:16 +0300

    Регистрация моделей в админке

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
+from .models import Department
+from .models import Employee
+from .models import TaskStage
+from .models import TaskStatus
+from .models import Task
+from .models import Comment
+
+
 # Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskStage)
+class TaskStageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskStatus)
+class TaskStatusAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
