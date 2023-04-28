eb31c4221087c091d4c3a0fd4c5a3c7102dc9aad
Константин Смуров <Phantom2525@gmail.com>
2023-04-28 09:55:37 +0300

    Кастомизация админки,  задачи

--- tasks/admin.py
+++ tasks/admin.py
@@ -32,7 +32,11 @@ class TaskStatusAdmin(admin.ModelAdmin):
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    pass
+    raw_id_fields = (
+        'accomplices',
+        'watchers',
+        'subscribers',
+    )
 
 
 @admin.register(Comment)
