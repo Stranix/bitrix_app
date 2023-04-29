82c4d0bbff0c5776cabeae26197b94abe5b7e132
Stranix <Phantom2525@gmail.com>
2023-04-29 16:56:59 +0300

    Рефактор менеджер команды fetch_bitrix_task

--- tasks/management/commands/fetch_bitrix_tasks.py
+++ tasks/management/commands/fetch_bitrix_tasks.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand
 
 from tasks.models import Employee, Task, TaskStage, TaskStatus
 from tasks.schemas.BitrixTaskSchema import BitrixTask
-from tasks.utils import fetch_bitrix_tasks_by_responsible_employee
+from tasks import utils
 
 logger = logging.getLogger(__name__)
 
@@ -13,94 +13,76 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         rnd_employees = Employee.objects.filter(department_id=1)
         for employee in rnd_employees:
-            tasks = fetch_bitrix_tasks_by_responsible_employee(
+            tasks = utils.fetch_bitrix_tasks_by_responsible_employee(
                 employee.btrx_id
             )
             self._save_in_db(tasks)
 
-    def _save_in_db(self, tasks: list[BitrixTask]):
-        users_not_found = []
-        for task in tasks:
-            print('Задача - ', task.id)
-            print('task_stage - ', task.stage.value)
-            print('task_status - ', task.status.value)
-            task_stage = TaskStage.objects.get(
-                btrx_stage_id=task.stage.value
-            )
-
-            task_status = TaskStatus.objects.filter(
-                btrx_status_id=task.status.value
-            ).first()
-
-            try:
-                task_creator = Employee.objects.get(btrx_id=task.created_id)
-            except Employee.DoesNotExist:
-                users_not_found.append(task.created_id)
-                logger.error('Employee with id %s not found', task.created_id)
+    def _save_in_db(self, bitrix_tasks: list[BitrixTask]):
+        for bitrix_task in bitrix_tasks:
+            if utils.get_task_by_id(bitrix_task.id):
+                logger.warning('Задача уже есть в базе. Пропускаем')
                 continue
 
-            task_responsible = Employee.objects.get(
-                btrx_id=task.responsible_id
+            task_creator = utils.get_employee_by_bitrix_id(
+                bitrix_task.created_id
             )
 
-            task_accomplices = Employee.objects.filter(
-                btrx_id__in=task.accomplices
+            task_responsible = utils.get_employee_by_bitrix_id(
+                bitrix_task.responsible_id
             )
 
-            task_watchers = Employee.objects.filter(
-                btrx_id__in=task.watchers
-            )
-
-            task_parent = None
-            if task.parent_id:
-                try:
-                    task_parent = Task.objects.get(bitrix_id=task.parent_id)
-                except Task.DoesNotExist:
-                    logger.warning('Task %s not exist', task.parent_id)
-
-            task_group_id = task.group_id if task.group_id else 0
-            task_closed_by = None
-            if task.closed_by_id:
-                try:
-                    task_closed_by = Employee.objects.get(
-                        btrx_id=task.closed_by_id
-                    )
-                except Employee.DoesNotExist:
-                    users_not_found.append(task.closed_by_id)
-                    logger.error(
-                        'Employee with id %s not found',
-                        task.closed_by_id
-                    )
-                    continue
-
-            task_subscribers = task_watchers
-
-            try:
-                Task.objects.get(bitrix_id=task.id)
-                continue
-            except Task.DoesNotExist:
-                task_closed_date_time = None if not task.closed_date_time else task.closed_date_time
-                task_in_db = Task.objects.create(
-                    bitrix_id=task.id,
-                    subject=task.subject,
-                    description=task.description,
-                    stage=task_stage,
-                    status=task_status,
-                    result_comment=None,
-                    creator=task_creator,
-                    responsible=task_responsible,
-                    parent_task=task_parent,
-                    group_id=task_group_id,
-                    files_url=None,
-                    create_at=task.created_date,
-                    start_at=task.created_date,
-                    duration_fact_time=task.duration_fact_time,
-                    closed_by=task_closed_by,
-                    closed_date_time=task_closed_date_time,
-                    last_activity_date=task.last_activity_date,
+            logger.debug('task_creator: %s', task_creator)
+            logger.debug('task_responsible: %s', task_responsible)
+            if not task_creator:
+                logger.warning(
+                    'Не найден постановщик %s',
+                    bitrix_task.created_id
+                )
+                task_creator = utils.get_user_from_bitrix_api(
+                    bitrix_task.created_id
                 )
 
-                task_in_db.accomplices.set(task_accomplices)
-                task_in_db.watchers.set(task_watchers)
-                task_in_db.subscribers.set(task_subscribers)
-        print(users_not_found)
+            task = Task()
+            task.bitrix_id = bitrix_task.id
+            task.subject = bitrix_task.subject
+            task.description = bitrix_task.description
+            task.group_id = bitrix_task.group_id
+
+            task.stage = utils.get_stage_by_btrx_id(
+                bitrix_task.stage.value
+            )
+            task.status = utils.get_status_by_btrx_id(
+                bitrix_task.status.value
+            )
+            task.creator = task_creator
+            task.responsible = task_responsible
+            task.create_at = bitrix_task.created_date
+
+            if bitrix_task.accomplices:
+                task_accomplices = Employee.objects.filter(
+                    btrx_id__in=bitrix_task.accomplices
+                )
+                task.accomplices.set(task_accomplices)
+
+            if bitrix_task.watchers:
+                task_watchers = Employee.objects.filter(
+                    btrx_id__in=bitrix_task.watchers
+                )
+                task.watchers.set(task_watchers)
+
+            task.parent_task = bitrix_task.parent_id
+            task.group_id = task.group_id if task.group_id else 0
+            task.duration_fact_time = bitrix_task.duration_fact_time
+
+            if utils.task_is_closed(bitrix_task):
+                task.closed_by = utils.get_employee_by_bitrix_id(
+                    bitrix_task.closed_by_id
+                )
+
+            if bitrix_task.closed_date_time:
+                task.closed_date_time = bitrix_task.closed_date_time
+            task.last_activity_date = bitrix_task.last_activity_date
+
+            logger.debug('Перед сохранением: %s', task.__dict__)
+            task.save()
