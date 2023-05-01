1e8eb2887bea5c0723db17b4429efd0ca718e267
Stranix <Phantom2525@gmail.com>
2023-05-01 20:46:53 +0300

    Заполнение tasks.views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from tasks.models import Employee
+from tasks.models import Task
+from tasks.serializers import EmployeeSerializer
+from tasks.serializers import TaskSerializer
+from rest_framework import generics
+
+logger = logging.getLogger(__name__)
+
+
+class EmployeesList(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class TasksList(generics.ListCreateAPIView):
+    queryset = Task.objects.all().order_by('-bitrix_id')
+    serializer_class = TaskSerializer
