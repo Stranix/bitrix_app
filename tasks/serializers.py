580b5546ea05de7df0a4b68c2f1b92087dab3d8d
Stranix <Phantom2525@gmail.com>
2023-05-04 15:47:34 +0300

    Перестроение сериализаторов
    Добавление сериализатор для менеджера команд
    Оптимизация для связанных полей в задачах

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,6 +4,8 @@ from rest_framework import serializers
 from tasks.models import Employee
 from tasks.models import Department
 from tasks.models import Task
+from tasks.models import TaskStage
+from tasks.models import TaskStatus
 
 logger = logging.getLogger(__name__)
 
@@ -20,12 +22,85 @@ class EmployeeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class TaskSerializer(serializers.Serializer):
-    bitrix_id = serializers.IntegerField()
-    subject = serializers.CharField()
+class TaskListSerializer(serializers.ModelSerializer):
+    accomplices = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='full_name'
+    )
 
-    def create(self, validated_data):
-        return Task(**validated_data)
+    watchers = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='full_name'
+    )
 
-    def update(self, instance, validated_data):
-        return instance
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    accomplices = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='full_name'
+    )
+
+    watchers = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='full_name'
+    )
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class BitrixTaskSerializer(serializers.ModelSerializer):
+    stage = serializers.SlugRelatedField(
+        queryset=TaskStage.objects,
+        slug_field='btrx_stage_id'
+    )
+    status = serializers.SlugRelatedField(
+        queryset=TaskStatus.objects,
+        slug_field='btrx_status_id'
+    )
+
+    creator = serializers.SlugRelatedField(
+        queryset=Employee.objects,
+        slug_field='btrx_id'
+    )
+    responsible = serializers.SlugRelatedField(
+        queryset=Employee.objects,
+        slug_field='btrx_id'
+    )
+    accomplices = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        queryset=Employee.objects,
+        slug_field='btrx_id'
+    )
+    watchers = serializers.SlugRelatedField(
+        many=True,
+        queryset=Employee.objects,
+        slug_field='btrx_id'
+    )
+
+    closed_by = serializers.SlugRelatedField(
+        required=False,
+        queryset=Employee.objects,
+        slug_field='btrx_id'
+
+    )
+
+    subscribers = serializers.SlugRelatedField(
+        many=True,
+        queryset=Employee.objects,
+        slug_field='btrx_id'
+    )
+
+    class Meta:
+        model = Task
+        fields = '__all__'
