24d016d7a2f4ac7d5a1b67af8ab7afd89976e375
Stranix <Phantom2525@gmail.com>
2023-05-04 15:56:10 +0300

    Оптимизация выдачи результата по задачам.

--- tasks/views.py
+++ tasks/views.py
@@ -1,24 +1,31 @@
 import logging
 
-from tasks.models import Employee
-from tasks.models import Task
-from tasks.serializers import EmployeeSerializer
-from tasks.serializers import TaskSerializer
 from rest_framework import generics
+
+from tasks import serializers
+from tasks.models import Task
+from tasks.models import Employee
 
 logger = logging.getLogger(__name__)
 
 
 class EmployeesList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    serializer_class = serializers.EmployeeSerializer
 
 
 class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    serializer_class = serializers.EmployeeSerializer
 
 
 class TasksList(generics.ListCreateAPIView):
-    queryset = Task.objects.all().order_by('-bitrix_id')
-    serializer_class = TaskSerializer
+    queryset = Task.objects.prefetch_related(
+        'accomplices', 'watchers'
+    ).order_by('-bitrix_id')
+    serializer_class = serializers.TaskListSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.prefetch_related('accomplices', 'watchers')
+    serializer_class = serializers.TaskDetailSerializer
